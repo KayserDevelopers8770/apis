c26fa0ffb5abdee4d6b6b86fc530ac18fba7e543
KayserDevelopers8770 <development@kayser.cl>
2018-07-23 18:04:28 -0400

    2018-07-23-5

--- controllers/externalController.js
+++ controllers/externalController.js
@@ -38,7 +38,7 @@ module.exports = {
         when U_GSP_CADOCU not IN ('vtd','vab') then cast(sum(U_GSP_CATOTA) AS Int) end as  total,
       max(CONVERT(VARCHAR(10), getdate(),120)) as fechaActualizacion, max(cast(year(U_GSP_CADATA) as varchar(4)) + '-'+ cast(month(U_GSP_CADATA)as varchar(2))) as periodo
       from [@GSP_TPVCAP] t0  inner join OCRD t1 on t1.CardCode=t0.U_GSP_CACLIE 
-      where U_GSP_CADOCU not in ('vti_ag','vtim_ag','vtd_ag') and t1.GroupCode='6' and U_GSP_CADATA between DATEADD(DAY, -15 , GETDATE()) AND getdate()
+      where U_GSP_CADOCU not in ('vti_ag','vtim_ag','vtd_ag') and t1.GroupCode='6' and U_GSP_CADATA between (GETDATE()-(DAY(GETDATE())-1)) AND getdate()
       group by t1.LicTradNum, U_GSP_CADOCU
       `
       const pool = await sql.connect(stringConnection)
