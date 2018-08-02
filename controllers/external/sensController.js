75c83044920a3aff4b782925578b14e59c4bbb69
KayserDevelopers8770 <development@kayser.cl>
2018-08-02 18:07:01 -0400

    2018-08-02-2

--- controllers/external/sensController.js
+++ controllers/external/sensController.js
@@ -7,12 +7,12 @@ module.exports = {
     try{
       const stringConnection = 'mssql://wms:pjc3l1@192.168.0.17/WMSTEK_KAYSER'
       const query_get_sens_stock = `
-      SELECT S.itemCode, CASE WHEN STOCK.Cant IS NULL THEN 0 ELSE STOCK.Cant END AS cantidad
-      FROM  (SELECT t0.IdArticulo as sku, CAST(SUM(t0.Cantidad)-30 AS int) as cant 
+      SELECT S.itemCode, CASE WHEN (STOCK.Cant IS NULL OR STOCK.Cant < 0 ) THEN 0 ELSE STOCK.Cant END AS cantidad
+      FROM  (SELECT t0.IdArticulo as sku, CAST(SUM(t0.Cantidad)-5 AS int) as cant 
           FROM Existencia as t0 
           INNER JOIN Ubicacion as t1 ON t0.IdUbicacion=t1.IdUbicacion
           WHERE t0.IdAlmacen = '01' AND t0.IdUbicacion LIKE '01%' and t1.Nivel in ('1','2')
-          GROUP BY IdArticulo HAVING SUM(Cantidad)>5 ) AS STOCK
+          GROUP BY IdArticulo) AS STOCK
       RIGHT JOIN [192.168.0.13].[Stock].[dbo].[Kayser_OITM] as S ON STOCK.sku = S.itemCode COLLATE SQL_Latin1_General_CP1_CI_AS
       WHERE S.U_Marca='SENS' 
       ORDER BY S.itemCode
